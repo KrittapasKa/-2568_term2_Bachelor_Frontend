2a9accbca5c5741019f7ecf3fffba579f5ff915a
krittapas.ka <krittapas.ka@outlook.com>
2026-02-16 16:57:41 +0700

    Home Page (Carousel)

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-home',
-    imports: [],
+    imports: [RouterLink],
     templateUrl: './home.html',
     styleUrl: './home.css'
 })
 export class Home {
+    slides = [0, 1, 2];
+    currentSlide = signal(0);
 
+    nextSlide() {
+        this.currentSlide.set((this.currentSlide() + 1) % this.slides.length);
+    }
+
+    prevSlide() {
+        this.currentSlide.set((this.currentSlide() - 1 + this.slides.length) % this.slides.length);
+    }
+
+    goToSlide(index: number) {
+        this.currentSlide.set(index);
+    }
 }
