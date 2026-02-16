9953cb6f948d3c3cab1ac26f46ef0e82469f8f5f
krittapas.ka <krittapas.ka@outlook.com>
2026-02-16 16:40:24 +0700

    RouterLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Contact } from './contact/contact';
+import { About } from './about/about';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: 'contact', component: Contact },
+    { path: 'about', component: About },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+];
